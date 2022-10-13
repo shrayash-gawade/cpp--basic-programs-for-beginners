e14dbb727ae8e03274a0b828c552e173d02aef49
Shrayash_Gawade <115636605+shrayash-gawade@users.noreply.github.com>
2022-10-13 11:58:19 +0530

    Add files via upload

---
+++ ProgramToFindSumOfNNumners.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n,sum;
+    cout<<"enter n:"<<endl;
+    cin>>n;
+    sum=(n*(n+1))/2;
+    cout<<"the  sum is :"<<sum<<endl;
+    return 0;
+
+}
