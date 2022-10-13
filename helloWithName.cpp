4d399143ea6fdbc1c57c398eff1c37f11ee50738
Shrayash_Gawade <115636605+shrayash-gawade@users.noreply.github.com>
2022-10-13 11:21:55 +0530

    Add files via upload

---
+++ helloWithName.cpp
@@ -0,0 +1,32 @@
+// 1st PROGRAM FOR NAME 
+
+
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"enter name:";
+    cin>>name;
+    cout<<"Hello "<<name<<endl;
+    return 0;
+
+}
+
+
+
+// SECOND PROGRAM FOR NAME WITH (GETLINE FUNCTION )
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     string name;
+//     cout<<"enter name:";
+//     getline(cin,name); //here we use getline function to print both name as well as surname the above program will print only 1st name 
+//     cout<<"Hello "<<name<<endl;
+//     return 0;
+
+// }
