96944fef4272f6e382c2395d8bd67b712397f90e
Shrayash_Gawade <115636605+shrayash-gawade@users.noreply.github.com>
2022-10-13 12:08:44 +0530

    Add files via upload

---
+++ IncrementOperator.cpp
@@ -0,0 +1,26 @@
+// basic increment problem 
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int i=6;
+//     i++;
+//     cout<<i;
+//     return 0;
+// }
+
+
+// advance incremnt problem 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=5,sum ;
+    sum=(++i)*(i++)+i++;
+    cout<<sum;
+    return 0;
+
+}
