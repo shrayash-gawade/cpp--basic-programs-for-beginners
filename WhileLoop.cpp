697ab1f49f32efa8e0bee5e03f1f5eb34600cc8b
Shrayash_Gawade <115636605+shrayash-gawade@users.noreply.github.com>
2022-10-14 18:12:36 +0530

    Add files via upload

---
+++ WhileLoop.cpp
@@ -0,0 +1,20 @@
+// program to print numbers from 1 to 10 
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1,n;
+    cout<<"enter n:";
+    cin>>n;
+
+    while (i<=n)
+    {
+        cout<<i<<endl;
+        i++;
+        
+    }
+    return 0;
+    
+}
