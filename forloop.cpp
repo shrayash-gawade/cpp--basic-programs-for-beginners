e346dda17e20f734beb94341cfeacddf76fcd307
Shrayash_Gawade <115636605+shrayash-gawade@users.noreply.github.com>
2022-10-14 18:12:04 +0530

    Add files via upload

---
+++ forloop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    cout<<"enter n:";
+    cin>>n;
+    for (i=1;i<=10;i++)
+    {
+        cout<<i<<endl;
+
+    }
+    return 0;
+    
+}
