7d6e3e258f83749050db0875ec18aaca360f46a4
Shrayash_Gawade <115636605+shrayash-gawade@users.noreply.github.com>
2022-10-14 18:12:20 +0530

    Add files via upload

---
+++ dowhile.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1,n;
+    cout<<"enter n:";
+    cin>>n;
+    do
+    {
+        cout<<i<<endl;
+        i++;
+    } while (i<=10);
+    return 0;
+    
+}
