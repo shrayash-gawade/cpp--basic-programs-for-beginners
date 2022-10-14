0c7f09a2395b46d6131221504d7a4e1a8f09cb85
Shrayash_Gawade <115636605+shrayash-gawade@users.noreply.github.com>
2022-10-14 17:49:04 +0530

    Add files via upload

---
+++ BitwiseOperators.cpp
@@ -0,0 +1,93 @@
+
+// BITWISE AND OPERATOR (&)
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b,c;
+//     cout<<"enter a and b :"
+//     cin>>a>>b;
+//     c=a&b;
+//     cout<<c;
+//     return 0;
+
+// }
+
+
+
+// BITWISE OR(|)
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b,c;
+//     cout<<"enter a and b:";
+//     cin>>a>>b;
+//     c=a|b;
+//     cout<<c;
+//     return 0;
+// }
+
+
+// bitwise n=Xor(^)
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b,c;
+//     cout<<"enter a and b:";
+//     cin>>a>>b;
+//     c=a^b;
+//     cout<<c;
+//     return 0;
+// }
+
+
+
+
+// BITWISE NOT(~)
+
+#include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b;
+//     cout<<"enter a :";
+//     cin>>a;
+//     b=~a;
+//     cout<<b;
+//     return 0;
+// }
+
+
+// right  shift bitwise 
+
+// #include<iostream>
+// #include<bitset>
+// using namespace std;
+// int main()
+// {
+//     int b;
+//     cout<<"enter binary value :";
+//     cin>>b;
+
+//     cout<<(b>>1);
+//     return 0;
+// }
+
+
+// left shift bitwise 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter a:";
+    cin>>a;
+    cout<<(a<<1);
+    return 0;
+}
